e2802e12f4f610c6da30393dea051a1a8d9b3272
rajkrrai <64032301+rajkrrai@users.noreply.github.com>
2021-12-07 20:17:17 +0100

    responsive website for mobile

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import "./Media.css";
 import {
@@ -14,56 +15,65 @@ import Awards from "./components/Awards";
 import Education from "./components/Education";
 import Volunteer from "./components/Volunteer";
 function App() {
+  useEffect(() => {
+    const toggleButton = document.getElementsByClassName("toggle-button")[0];
+    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+    toggleButton.addEventListener("click", () => {
+      navbarLinks.classList.toggle("active");
+    });
+  });
+  // const toggleMenu = () => {
+  //   console.log("menu clicked");
+  //   document.getElementById("nav-menulist").classList.toggle("show");
+  //   // document.getElementById("menuLink").style.display = "block";
+  // };
   return (
     <Router>
-      <nav className="nav1">
-        <div className="navbar-container">
-          <div className="name-tag">
-            <span className="lessThan">&lt;</span>
-            <span className="nameInNav">Rajeh Kumar</span>
-            <span className="grtThan">/&gt;</span>
-          </div>
-          <div className="navbar">
-            <i className="fas fa-bars menubar"></i>
-            <ul className="navLinks">
-              <li>
-                <a href="/" exact="true" className="nav-link-items">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#workex" className="nav-link-items">
-                  Work-Ex
-                </a>
-              </li>
-              <li>
-                <a href="#awards" className="nav-link-items">
-                  Awards
-                </a>
-              </li>
-              <li>
-                <a href="#skills" className="nav-link-items">
-                  Skills
-                </a>
-              </li>
-              <li>
-                <a href="#education" className="nav-link-items">
-                  Education
-                </a>
-              </li>
-              <li>
-                <a href="#volunteer" className="nav-link-items">
-                  Volunteerships
-                </a>
-              </li>
-            </ul>
-          </div>
+      <nav class="navbar">
+        <div class="brand-title">
+          <span className="lessThan">&lt;</span>
+          <span className="nameInNav">Rajeh Kumar</span>
+          <span className="grtThan">/&gt;</span>
+        </div>
+        <a href="#" class="toggle-button">
+          <i className="fas fa-bars menubar"></i>
+        </a>
+        <div class="navbar-links">
+          <ul>
+            <li>
+              <a href="/" exact="true" className="nav-link-items">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#workex" className="nav-link-items">
+                Work-Ex
+              </a>
+            </li>
+            <li>
+              <a href="#awards" className="nav-link-items">
+                Awards
+              </a>
+            </li>
+            <li>
+              <a href="#skills" className="nav-link-items">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#education" className="nav-link-items">
+                Education
+              </a>
+            </li>
+            <li>
+              <a href="#volunteer" className="nav-link-items">
+                Volunteerships
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
-      {/* <Routes>
-          <Route path="/" element={Homepage}></Route>
-          <Route path="/workex" element={Workex}></Route>
-        </Routes> */}
       <Homepage />
       <Workex />
       <Skills />
