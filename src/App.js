b44431a4c1c0f92874aff8c96a6a1cd55a80a09d
rajkrrai <64032301+rajkrrai@users.noreply.github.com>
2021-12-13 02:20:39 +0100

    navbar sticky for mob and desktop site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "./Media.css";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 import Homepage from "./components/Homepage";
 import Workex from "./components/Workex";
@@ -10,56 +10,100 @@ import Awards from "./components/Awards";
 import Education from "./components/Education";
 import Volunteer from "./components/Volunteer";
 import Footer from "./components/Footer";
-import Projects from "./components/Projects";
-function App() {
-  useEffect(() => {
-    const toggleButton = document.getElementsByClassName("toggle-button")[0];
-    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+// import Projects from "./components/Projects";
+const App = () => {
+  // const [navabr, setScroll] = useState(false);
 
-    toggleButton.addEventListener("click", () => {
-      navbarLinks.classList.toggle("active");
+  useEffect(() => {
+    const toggleButton = document.getElementById("toggle-button");
+    toggleButton.addEventListener("click", (e) => {
+      onLinkClick();
     });
   });
+
+  const onLinkClick = (e) => {
+    const navbarLinks = document.getElementById("navbar-links");
+    navbarLinks.classList.toggle("active");
+  };
+
+  window.addEventListener("scroll", (e) => {
+    // console.log("scroll event", window.scrollY);
+    var nav = document.querySelector("nav");
+
+    if (window.scrollY > 22) {
+      // setScroll(true);
+      nav.classList.add("sticky");
+    } else {
+      //setScroll(false);
+      nav.classList.remove("sticky");
+    }
+  });
+
   return (
-    <Router>
-      <nav class="navbar container">
-        <div class="brand-title">
+    <div>
+      <nav className="navbar">
+        <div className="brand-title">
           <span className="lessThan">&lt;</span>
           <span className="nameInNav">Rajeh Kumar</span>
           <span className="grtThan">/&gt;</span>
         </div>
-        <a href="#!" class="toggle-button">
+        <a href="#!" className="toggle-button" id="toggle-button">
           <i className="fas fa-bars menubar"></i>
         </a>
-        <div class="navbar-links">
+        <div className="navbar-links" id="navbar-links">
           <ul>
             <li>
-              <a href="/" exact="true" className="nav-link-items">
+              <a
+                href="/"
+                exact="true"
+                className="nav-link-items"
+                onClick={onLinkClick}
+              >
                 Home
               </a>
             </li>
             <li>
-              <a href="#workex" className="nav-link-items">
+              <a
+                href="#workex"
+                className="nav-link-items"
+                onClick={onLinkClick}
+              >
                 Work
               </a>
             </li>
             <li>
-              <a href="#awards" className="nav-link-items">
+              <a
+                href="#awards"
+                className="nav-link-items"
+                onClick={onLinkClick}
+              >
                 Awards
               </a>
             </li>
             <li>
-              <a href="#skills" className="nav-link-items">
+              <a
+                href="#skills"
+                className="nav-link-items"
+                onClick={onLinkClick}
+              >
                 Skills
               </a>
             </li>
             <li>
-              <a href="#education" className="nav-link-items">
+              <a
+                href="#education"
+                className="nav-link-items"
+                onClick={onLinkClick}
+              >
                 Education
               </a>
             </li>
             <li>
-              <a href="#volunteer" className="nav-link-items">
+              <a
+                href="#volunteer"
+                className="nav-link-items"
+                onClick={onLinkClick}
+              >
                 Volunteerships
               </a>
             </li>
@@ -69,14 +113,14 @@ function App() {
 
       <Homepage />
       <Workex />
-      {/* <Projects /> */}
+
       <Skills />
       <Awards />
       <Education />
       <Volunteer />
       <Footer />
-    </Router>
+    </div>
   );
-}
+};
 
 export default App;
